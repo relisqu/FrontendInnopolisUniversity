69072493c7abe7effe0a68706e75c9b0179fd5b7
Kopeikina Anna <kopkopkop47@gmail.com>
2023-03-15 19:49:00 +0300

    changed to svelte

---
+++ src/lib/socials.svelte
@@ -0,0 +1,10 @@
+
+<div class="socials">
+    <h1>Where to find me?</h1>
+    <a id="gh-link" href="https://github.com/relisqu"><img class="color-icon" alt = "github"
+        src="https://img.icons8.com/ios-filled/50/000000/github.png" /></a>
+    <a id="email" href="mailto:relisqu@gmail.com"><img class="color-icon"  alt = "google_email"
+        src="https://img.icons8.com/ios-filled/50/000000/gmail" /></a>
+    <a id="tg-link" href="https://t.me/relisqu"><img class="color-icon" alt = "telegram"
+        src="https://img.icons8.com/ios-filled/50/000000/telegram.png" /></a>
+  </div>
